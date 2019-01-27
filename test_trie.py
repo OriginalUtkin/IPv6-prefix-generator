357519f5156af6242e4b4b113a0a5ad2742578c8
Kirill <kirill@MacBook-Pro.local>
2019-01-27 01:46:49 +0100

    Add new statement to check_depth_distribution function

--- test_trie.py
+++ test_trie.py
@@ -14,13 +14,16 @@ binary_trie.preorder(binary_trie.root_node, "statistic")
 # print(binary_trie.prefix_leaf_nodes)
 # print(binary_trie.prefix_nodes)
 
+new_prefixes_param = 100
+
 
 def check_depth_distribution(trie: Trie, new_depth_distribution: dict):
     # Number of new prefixes depends on :param new_dept_distribution
     new_prefixes_num = 0
-    current_depth_destribution = trie.prefix_nodes
+    current_depth_distribution = trie.prefix_nodes
+
     for depth, prefixes_num in new_depth_distribution.items():
-        current_value = current_depth_destribution.get(depth)
+        current_value = current_depth_distribution.get(depth)
 
         if prefixes_num < 0:
             raise ValueError("Number of prefixes can't be less than zero")
@@ -28,23 +31,24 @@ def check_depth_distribution(trie: Trie, new_depth_distribution: dict):
         if depth < 0:
             raise ValueError("Level value can't be less than zero")
 
-
         if current_value is None:  # level doesn't exist in trie
-            new_prefixes_num += prefixes_num
             current_value = 0
+            new_prefixes_num += prefixes_num
 
         if prefixes_num - current_value < 0:
             raise ValueError("Number of prefixes on generated depth can't be less than current ")
-
 
         if prefixes_num - current_value > 0 and not trie.get_depths(depth):
             raise ValueError(f"There is no leaf prefixes in trie for generate new prefixes on {depth} depth")
 
         new_prefixes_num += prefixes_num - current_value
 
+    if new_prefixes_num > new_prefixes_param:
+        raise ValueError("Generated prefixes num is greater than expected")
+
 # Need fix -> if depth 1 = 1(FIXED_ but need tests)
 check_depth_distribution(binary_trie,  {0: 1,
-        1: 1,
+        1: 0,
         2: 1,
         3: 4,
         4: 2,
