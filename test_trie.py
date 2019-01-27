6c220179022ae9954dfd6d75c41c758f62fe11c0
Kirill <kirill@MacBook-Pro.local>
2019-01-27 01:39:57 +0100

    fix check_distr_function logic

--- test_trie.py
+++ test_trie.py
@@ -15,12 +15,10 @@ binary_trie.preorder(binary_trie.root_node, "statistic")
 # print(binary_trie.prefix_nodes)
 
 
-
 def check_depth_distribution(trie: Trie, new_depth_distribution: dict):
     # Number of new prefixes depends on :param new_dept_distribution
     new_prefixes_num = 0
     current_depth_destribution = trie.prefix_nodes
-
     for depth, prefixes_num in new_depth_distribution.items():
         current_value = current_depth_destribution.get(depth)
 
@@ -30,25 +28,26 @@ def check_depth_distribution(trie: Trie, new_depth_distribution: dict):
         if depth < 0:
             raise ValueError("Level value can't be less than zero")
 
+
         if current_value is None:  # level doesn't exist in trie
             new_prefixes_num += prefixes_num
-            continue
+            current_value = 0
 
         if prefixes_num - current_value < 0:
-            print(current_value)
             raise ValueError("Number of prefixes on generated depth can't be less than current ")
+
 
         if prefixes_num - current_value > 0 and not trie.get_depths(depth):
             raise ValueError(f"There is no leaf prefixes in trie for generate new prefixes on {depth} depth")
 
         new_prefixes_num += prefixes_num - current_value
 
-
-check_depth_distribution(binary_trie,  {1: 1,
-        2: 0,
-        3: 1,
-        4: 4,
-        5: 2,
-        6: 0,
-        7: 2,
+# Need fix -> if depth 1 = 1(FIXED_ but need tests)
+check_depth_distribution(binary_trie,  {0: 1,
+        1: 1,
+        2: 1,
+        3: 4,
+        4: 2,
+        5: 0,
+        6: 2,
         })
